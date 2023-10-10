af44c202895b987c3e7b6a5264dced140c02fbd2
jzmx <jorge@zaldivar.mx>
2023-10-10 16:19:06 +0000

    Re-enter centennial leap year check

--- bisiesto.py
+++ bisiesto.py
@@ -6,7 +6,7 @@ Determinar si un año es bisiesto.
 anho = int(input("Introduzca un año: "))
 
 # Proceso
-if anho % 4 == 0 and anho % 100 != 0:
+if anho % 400 == 0 or anho % 4 == 0 and anho % 100 != 0:
     bisiesto = "sí"
 else:
     bisiesto = "no"
